78917c98c8da7ef287eeb5667340d3295d0faa5f
Jeff Kuure <kuure@msu.edu>
2025-11-21 11:22:56 -0500

    Inital commit, kind of late

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,11 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            DayForecast(day: "Tue", isRainy: true, high: 80, low: 40)
+        }
+    }
+}
+
+
+struct DayForecast: View {
+    
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        }
+        else {
+            return "sun.max.fill"
+        }
+    }
+
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        }
+        else {
+            return Color.yellow
+        }
+    }
+
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(day)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+            Text("High: \(high)º")
+            Text("Low: \(low)º")
         }
         .padding()
     }
